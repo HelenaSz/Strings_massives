7e6e7e7006287f3cc5550a716c5d417cf734d6d4
Neto work <lenas_2003@mail.ru>
2023-11-22 00:06:11 +0200

    Corrections in the project-2

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -13,11 +13,8 @@ public class StatsService {
 
     public int avgAllSales(int[] sales) { //средняя сумма продаж в месяц
         int avg;
-        int sum = 0;
+        int sum = sumAllSales(sales);
 
-        for (int sale : sales) {
-            sum += sale;
-        }
         avg = sum / sales.length;
 
         return avg;
@@ -31,7 +28,6 @@ public class StatsService {
                 minMonth = i; // запомним его как минимальный
             }
         }
-
         return minMonth + 1; // месяца нумеруются с 1, а индексы массива с 0, нужно сдвинуть ответ на 1
     }
 
@@ -43,45 +39,30 @@ public class StatsService {
                 maxMonth = i;
             }
         }
-
         return maxMonth + 1;
     }
 
     public int salesLowAverage(int[] sales) { //количество месяцев, в которых продажи были ниже среднего
-        int avg;
-        int sum = 0;
+        int avg = avgAllSales(sales);
         int lowAverage = 0;
 
         for (int sale : sales) {
-            sum += sale;
-        }
-
-        avg = sum / sales.length;
-
-        for (int sale : sales)
             if (sale < avg) {
                 lowAverage++;
             }
-
+        }
         return lowAverage;
     }
 
     public int salesHighAverage(int[] sales) { //количество месяцев, в которых продажи были выше среднего
-        int avg;
-        int sum = 0;
+        int avg = avgAllSales(sales);
         int highAverage = 0;
 
         for (int sale : sales) {
-            sum += sale;
-        }
-
-        avg = sum / sales.length;
-
-        for (int sale : sales)
             if (sale > avg) {
                 highAverage++;
             }
-
+        }
         return highAverage;
     }
 }
